84ff3111408f32f683fe67f23bf42991fc3d2eaa
Tihomir <trecic@gmail.com>
2023-05-24 10:35:08 +0200

    Added .cs files

---
+++ Dvd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace TestAppBootcamp
+{
+	public class Dvd: LibraryItem, ISubtitle
+	{
+        public float DurationMinutes { get; set; }
+        public bool BlankCover { get; set;  }
+
+
+        public string Subtitle()
+        {
+            string subtitleExample = "This is subtitles for DVD";
+            return subtitleExample;
+        }
+
+        public override string ToString()
+        {
+            return $"Dvd id: {IdLibraryItem} \nDvd Author: {AuthorName} \nPublication Year: {PublicationYear} \nDration: {DurationMinutes} \nBlank Cover: {BlankCover}";
+        }
+
+    }
+}
+
---
+++ ISubtitle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TestAppBootcamp
+{
+	public interface ISubtitle
+	{
+		string Subtitle();
+ 
+		     
+	}
+}
+
